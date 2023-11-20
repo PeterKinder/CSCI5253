f75e428603524c004320218a955696a81825bca5
Peter Kinder <kinderp@colorado.edu>
2023-11-19 18:13:26 -0700

    Second Part

---
+++ dags/etl_scripts/load.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import os
+from sqlalchemy.dialects.postgresql import insert
+
+def load_data(table_file, table_name, key):
+    db_url = os.environ['DB_URL']
+    conn = create_engine(db_url) 
+    def insert_on_conflict_nothing(table, conn, keys, data_iter):
+        data = [dict(zip(keys, row)) for row in data_iter]
+        stmt = insert(table.table).values(data).on_conflict_do_nothing(index_elements=[key])
+        result = conn.execute(stmt)
+        return result.rowcount 
+    pd.read_parquet(table_file).to_sql(table_name, conn, if_exists='append', index=False, method=insert_on_conflict_nothing)
+    print(f"Loaded {table_file} to {table_name} table")
--- dags/outcome_dag.py
+++ dags/outcome_dag.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from etl_scripts.transform import transform_data
+from etl_scripts.load import load_data
 from datetime import datetime
 import os
 SOURCE_URL= 'https://data.austintexas.gov/api/views/9t4d-g238/rows.csv?date=20231118&accessType=DOWNLOAD'
@@ -11,7 +12,7 @@ CSV_TARGET_FILE = CSV_TARGET_DIR + '/outcomes_{{ ds }}.csv'
 PQ_TARGET_DIR = AIRFLOW_HOME + '/data/{{ ds }}/processed'
 with DAG(
     dag_id="outcomes_dag",
-    start_date = datetime(2023, 11, 18),
+    start_date = datetime(2023, 11, 20),
     schedule_interval = "@daily"
 ) as dag:
     extract = BashOperator(
@@ -26,4 +27,13 @@ with DAG(
             'target_dir': PQ_TARGET_DIR
         }
     )
-    extract >> transform
+    load_outcome_animal_dim = PythonOperator(
+        task_id="load_outcome_animal_dim",
+        python_callable=load_data,
+        op_kwargs={
+            'table_file': PQ_TARGET_DIR + '/outcome_animal_dim.parquet',
+            'table_name': 'outcome_animal_dim',
+            'key': 'animal_id'
+        }
+    )
+    extract >> transform >> load_outcome_animal_dim
